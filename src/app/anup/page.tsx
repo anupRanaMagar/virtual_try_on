edc078c89e2dabdb4f696ceb5c9f917bcf5ee443
apple <apple@apples-MacBook-Air.local>
2025-02-06 11:26:03 +0545

    inital commit

---
+++ src/app/anup/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Anup</div>;
+};
+
+export default page;
---
+++ src/components/ImageInput.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ImageInput = () => {
+  return <></>;
+};
+
+export default ImageInput;
